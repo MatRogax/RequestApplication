4df93074114090080e3cb3a52105550c7305e744
Matheus Rogato <matheusrogato@gmail.com>
2024-11-26 20:36:58 -0300

    refactor: refactoração do modulo de usuario

--- src/base/modules/user.module.ts
+++ src/base/modules/user.module.ts
@@ -2,10 +2,10 @@ import { UserController } from "@controllers/user.controller";
 import { PrismaModule } from "@modules/prisma.module";
 import { Module } from "@nestjs/common";
 import { UserRepository } from "../repositories/user.repository";
-import { AuthModule } from "./auth.module";
+import { AuthModule } from "@modules/auth.module";
 
 @Module({
-  imports: [PrismaModule,AuthModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [UserController],
   providers: [UserRepository],
   exports: [UserRepository],
